3307b4d5fa87ff77d4cddadbd35ce146f85d7d7c
HappyToby <t2103034327@163.com>
2025-03-27 00:11:58 +0800

    客户端菜单

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //当前客户端模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       -1,
 	}
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
 	if err != nil {
@@ -26,6 +28,46 @@ func NewClient(serverIp string, serverPort int) *Client {
 	}
 	client.conn = conn
 	return client
+}
+
+// 客户端菜单选项
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.公屏聊天")
+	fmt.Println("2.私聊")
+	fmt.Println("3.更改用户名")
+	fmt.Println("0.退出")
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>请输入合法范围的数字<<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+
+		}
+
+		switch client.flag {
+		case 1:
+			//公屏聊天
+			fmt.Println("公聊模式选择")
+			break
+		case 2:
+			//私聊
+			fmt.Println("私聊模式选择")
+			break
+		case 3:
+			//修改名称
+			fmt.Println("修改名称")
+			break
+		}
+	}
 }
 
 // 用户接收和初始化ip and port
@@ -49,7 +91,5 @@ func main() {
 	}
 	fmt.Println(">>>服务器链接成功...")
 	//启动客户端的业务
-	select {
-	default:
-	}
+	client.Run()
 }
